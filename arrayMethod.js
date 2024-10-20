a555e034256df74682bf0412fc1351f98be10c3c
Strom <110904918+JuliStrom@users.noreply.github.com>
2024-10-20 21:34:26 +0300

    init: Create repo and first file arrayMethod.js

---
+++ arrayMethod.js
@@ -0,0 +1,9 @@
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+let pushed = rows.push();
+let popped = rows.pop();
+console.log(pushed);
+console.log(popped);
+console.log(rows);
